c0c6197c2512f09976e0dcd88a594ef6526ebdcc
Serge Victor <github@random.re>
2015-02-08 10:06:19 +0700

    - adding first batches of files.

---
+++ language/en/common.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* @package phpBB Extension - Acme Demo
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'NAGIOS_ACTIVE_USERS'           => 'Active users:',
+
+        'ACP_NAGIOS_TITLE'		=> 'Nagios Module',
+	'ACP_NAGIOS'			=> 'Settings',
+        'ACP_NAGIOS_STATUS'		=> 'Should Nagios health monitoring be active?',
+	'ACP_NAGIOS_SETTING_SAVED'	=> 'Settings have been saved successfully!',
+));
