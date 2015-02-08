213bdd7d86cd321ec414d80b3a2bf8af2748f542
Serge Victor <github@random.re>
2015-02-08 20:16:27 +0700

    - adding first batches of files.

--- controller/main.php
+++ controller/main.php
@@ -93,6 +93,8 @@ class main
             // Checking the phpBB version
             $version_helper = $this->phpbb_container->get('version_helper');
 
+            print($version_helper)
+
             // Get translation
             $this->user->add_lang_ext('ser/nagios', 'common');
 
