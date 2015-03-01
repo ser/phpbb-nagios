ccd2c6d7b9e8ae440cca759df2a7d26237a984b6
Serge Victor <github@random.re>
2015-03-02 00:32:32 +0700

    some progress

--- language/en/common.php
+++ language/en/common.php
@@ -21,7 +21,8 @@ $lang = array_merge($lang, array(
 	'NAGIOS_ACTIVE_USERS_TEXT'      => 'Active users:',
 	'NAGIOS_FORUM_VERSION'		=> 'Forum Ver',
         'ACP_NAGIOS_TITLE'		=> 'Nagios Module',
-	'ACP_NAGIOS'			=> 'Settings',
+        'ACP_NAGIOS'			=> 'Settings',
+        'ACP_NAGIOS_ENABLE'		=> 'Do you want to activate Nagios health monitoring?',
         'ACP_NAGIOS_STATUS'		=> 'Should Nagios health monitoring be active?',
 	'ACP_NAGIOS_SETTING_SAVED'	=> 'Settings have been saved successfully!',
 ));
